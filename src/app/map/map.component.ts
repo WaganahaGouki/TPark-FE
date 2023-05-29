6a29779e9468d03647e395e2ad101106db18b3d3
Casian-Gabriel Șuli <ksi0711@yahoo.com>
2023-05-29 16:16:39 +0300

    Set the initial center and zoom for the map

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -5,6 +5,12 @@ import { Component } from '@angular/core';
   templateUrl: './map.component.html',
   styleUrls: ['./map.component.css']
 })
-export class MapComponent {
 
+export class MapComponent {
+    public zoom = 14;
+
+    public position = {
+      lat: 45.755968,
+      lng: 21.228967
+    };
 }
